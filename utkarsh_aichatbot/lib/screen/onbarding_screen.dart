d1b46e10ab52e0888c923e71e15db6fba148ec9f
Utkarsh Khajuria <157891400+UtkarsHMer05@users.noreply.github.com>
2024-06-05 22:07:14 +0530

    commit

---
+++ utkarsh_aichatbot/lib/screen/onbarding_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:utkarsh_aichatbot/helper/global.dart';
+import 'package:utkarsh_aichatbot/model/onboard.dart';
+import 'package:utkarsh_aichatbot/screen/home_screen.dart';
+
+class OnbardingScreen extends StatelessWidget {
+  const OnbardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final c = PageController();
+    final list = [
+      //onboarding 1
+      Onboard(
+          title: "Ask Me Anything",
+          subtitle:
+              "I can be your best friend & you can ask me anything & I will help you!",
+          lottie: "ai_ask_me"),
+      //onboarding 2
+      Onboard(
+          title: "Imagination to Reality",
+          subtitle: "Just Imagine Something in your Mind and I will Create it ",
+          lottie: "ai_play")
+    ];
+
+    return Scaffold(
+      body: PageView.builder(
+        controller:  c,
+        itemCount: list.length,
+        itemBuilder: (ctx, ind) {
+          final isLast = ind == list.length-1 ;
+          return Column(
+        children: [
+          //lottie
+          Lottie.asset('assets/lottie/${list[ind].lottie}.json', height: mq.height * .7,),
+          //title
+          Text(
+            list[ind].title,
+            style: const TextStyle(
+                fontSize: 18, fontWeight: FontWeight.w900, letterSpacing: .5),
+          ),
+          //for adding space
+          SizedBox(
+            height: mq.height * .01,
+          ),
+          // const Spacer(),
+          //subtitle
+          SizedBox(
+            width: mq.width * .7,
+            child: Text(
+             list[ind].subtitle,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  fontSize: 13.5, letterSpacing: .5, color: Colors.black54),
+            ),
+          ),
+          const Spacer(),
+          //dots
+          Wrap(
+            spacing: 10,
+            children: List.generate(
+                list.length,
+                (i) => Container(
+                      width: i == ind ? 17 : 10,
+                      height: 8,
+                      decoration: BoxDecoration(
+                          color: i == ind ? Colors.blue : Colors.grey,
+                          borderRadius: const BorderRadius.all(Radius.circular(5))),
+                    )),
+          ),
+          const Spacer(),
+          //buttons
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: const StadiumBorder(),
+                  elevation: 0,
+                  textStyle: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w500),
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.black,
+                  minimumSize: Size(mq.width * .4, 50)),
+              onPressed: () {
+                if (isLast) {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=>const HomeScreen()));
+                  
+                }else{
+                  c.nextPage(duration: const Duration(milliseconds: 600), curve: Curves.ease);
+                }
+                
+
+              },
+              child: Text(isLast ? 'Finish':'Next')),
+          const Spacer(),
+        ],
+      );
+        },
+      ),
+    );
+  }
+}
--- utkarsh_aichatbot/lib/screen/splash_screen.dart
+++ utkarsh_aichatbot/lib/screen/splash_screen.dart
@@ -3,6 +3,9 @@
 import 'package:flutter/material.dart';
 import 'package:utkarsh_aichatbot/helper/global.dart';
 import 'package:utkarsh_aichatbot/screen/home_screen.dart';
+import 'package:utkarsh_aichatbot/screen/onbarding_screen.dart';
+import 'package:utkarsh_aichatbot/widget/custom_loading.dart';
+
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,8 +21,8 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     //waiting sometime for splash screen then moving to homescreen
     Future.delayed( const Duration (seconds: 2) ,(){
-Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=> const HomeScreen()));//material page route means routing one page to other page(navigating to it)
-    }
+Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=> const OnbardingScreen()));//material page route means routing one page to other page(navigating to it)
+    }//routing to the onboarding screen
     );
     
   }
@@ -30,14 +33,27 @@ Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=> const Hom
     mq = MediaQuery.sizeOf(context);
     return Scaffold(
 
-      body: Center(child: Card(
-        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-        child: Padding(
-          padding:  EdgeInsets.all(mq.width * .05),//adding dynamic size of the logo inside the card rather than const which will be better
-          child: Image.asset("assets/images/logo.png",width: mq.width * .45,),
-        ),//45% of the whole screen
-
-      ),
+      body: SizedBox(  // we are putting sizebox because to put the logo in middle screen instead of left side
+         width: double.maxFinite,
+        child: Column(
+          children: [
+            const Spacer(flex: 2,),
+            Card(
+              shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: Padding(
+                padding:  EdgeInsets.all(mq.width * .05),//adding dynamic size of the logo inside the card rather than const which will be better
+                child: Image.asset("assets/images/logo.png",width: mq.width * .45,),
+                
+              ),
+              
+              //45% of the whole screen
+            ),
+            const Spacer(),
+            CustomLoading(),
+            
+            const Spacer(),  //loading custom lottie screen
+          ],
+        ),
       ),
     );
   }
